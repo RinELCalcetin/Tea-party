af1869c662042407d51c3577af1614020a40ba48
Elena Blanco Fernández <elenablancofer@gmail.com>
2022-05-26 02:10:27 +0200

    comienzo del nuevo proyecto
    queda meter algunas imagener y buscar la manera de hacer el codigo

---
+++ Trabajo/js/script.js
@@ -0,0 +1,8 @@
+let ingrediente = document.getElementsByClassName('ingrediente')
+let HO = document.getElementById('HO')
+let FA =document.getElementById('FA')
+
+
+$('.ingrediente').draggable();
+$('.cajonT').droppable();
+
